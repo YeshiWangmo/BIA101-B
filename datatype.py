4d7cde9a8b0bf031a1ba70f848dde269a7ad3040
unknown <03230424.gcbs@rub.edu.bt>
2024-03-01 12:00:38 +0600

    datatype

---
+++ datatype.py
@@ -0,0 +1,11 @@
+var1 = 10 #integer 
+name ="Yeshi"
+name2 = "Wangmo"
+score = 10.1 # float/ decimals
+issheman= True # boolean 
+print(var1)
+var1 =20 
+print (var1)
+
+print(type(var1))
+print(type(name))
